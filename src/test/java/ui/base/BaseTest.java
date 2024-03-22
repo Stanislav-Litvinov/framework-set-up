6afc9ef275c82d1f924a223ba582d3675bc1ed9e
Stanislav Litvinov <slytvynov@gftrewards.com>
2024-03-22 11:10:23 +0400

    pom file and base_test class

---
+++ src/test/java/ui/base/BaseTest.java
@@ -0,0 +1,27 @@
+package ui.base;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class BaseTest {
+
+    public void setUp() {
+        WebDriverManager.chromiumdriver().setup();
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.headless = false;
+    }
+
+    @BeforeEach
+    public void init() {
+        setUp();
+    }
+
+    @AfterEach
+    public void tearDown() {
+          Selenide.closeWebDriver();
+    }
+}
